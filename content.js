d66ffab8dbdacd3e2ddf524eca7e34e178c77e38
dhruvaism <b170177@nitsikkim.ac.in>
2020-08-03 08:28:19 +0530

    auto date click implemented

--- content.js
+++ content.js
@@ -1,22 +1,40 @@
 console.log("Sarathi Slot Booking go...");
 
-//first appointment date
-var firstIndex = -1;
+//find appointment date slot
+var allDates = document.getElementsByTagName("td");
+var firstDate = -1;
+if(allDates){
+    for(j=0;j<allDates.length;j++){
+        if(allDates[j].style.color ===  "green"){
+            firstDate = j;
+            break;
+        }
+    }
+}
+if(firstDate!=-1){
+    var a = allDates[firstDate].getElementsByTagName("a");
+    if(a){
+        console.log(a[0].href);
+        window.location = a[0].href;
+    }
+}
 
+//first appointment time slot
+var firstTime = -1;
 //finding first appointment date
 var allInputRadio = document.getElementsByTagName('input');
 if(allInputRadio){
    for (i = 0; i < allInputRadio.length; i++) {
         if (allInputRadio[i].type == 'radio') {
-            firstIndex = i;
+            firstTime = i;
             break;
         }
     }
 }
 
-if(firstIndex!=-1){
+if(firstTime!=-1){
     //for auto select appointment date
-    allInputRadio[firstIndex].click();
+    allInputRadio[firstTime].click();
     //for auto book slot
     var bookSlot = document.getElementById('slotbtn');
     if(bookSlot){
@@ -30,6 +48,5 @@ if(finalForm){
    finalForm.submit();
 }
 
-
-
- 
+ //https://sarathi.parivahan.gov.in/slotscov12/fetchslotdet.do?date=19-08-2020
+///slotscov12/savellslotdet.do
